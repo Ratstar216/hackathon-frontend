4ce28dfcc1b736fe37425cc0df1dc7f0643eaf1b
Ratstar216 <130824137+Ratstar216@users.noreply.github.com>
2024-11-28 19:41:33 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Form from "./Form";
-import { useState, useEffect} from "react";
-import axios from "axios";
+import React from 'react';
+import { useState } from 'react';
+import { onAuthStateChanged } from "firebase/auth";
+import { fireAuth } from "./firebase";
+import './App.css';
+import Signout from './Signout';
+import SignupForm from './SignupForm';
+import SigninForm from './SigninForm';
+import Contents from './Contents';
 
 function App() {
-  type User = {
-    id: number;
-    name: string;
-    age: number; 
-  }
+  const [loginUser, setLoginUser] = useState(fireAuth.currentUser);
 
-  type FormData = {
-    name: string;
-    age: number;
-  }
-
-  const getUser = async () => {
-    try {
-      const response = await fetch('http://localhost:8000/user');
-  
-      if (!response.ok) {
-        throw new Error(`Failed to fetch users, status: ${response.status}`);
-      }
-  
-      const users = await response.json() as User[];
-      return users;
-    } catch (error) {
-      console.error('Fetch error:', error);
-    }
-  };
-
-  const [users, setUsers] = useState<User[]>([]);
-  const update = async () => {
-    const users = await getUser();
-    if (users === undefined) {
-      return;
-    }
-    setUsers(users);
-  }
-
-  useEffect(() => {
-    update();
-  }, []);
-
-  const handleSubmit = async (name: string, age: number) => {
-    if (!name) {
-      alert("Please enter name");
-      return;
-    }
-
-    if (name.length > 50) {
-      alert("Please enter a name shorter than 50 characters");
-      return;
-    }
-
-    if (age < 20 || age > 80) {
-      alert("Please enter age between 20 and 80");
-      return;
-    }
-    const formdata: FormData = {
-      name: name,
-      age: age
-    };
-
-    try {
-      const response = await axios.post('http://localhost:8000/user', formdata);
-      console.log('Response:', response);
-    } catch (error) {
-      console.error('Failed to post data', error);
-    }
-    console.log('Form data:', formdata);
-    update();
-  };
-
+  onAuthStateChanged(fireAuth, user => {
+    setLoginUser(user);
+  });
 
   return (
     <div className="App">
       <header className="App-header">
-       <p>User Register</p>
+       <p>Authentication</p>
       </header>
-      <Form onSubmit={handleSubmit} />
-      <div className="container">
-        {users.map((user: User) => (<div key={user.id} className="data">{user.name}, {user.age}</div>))}
+      <div className="Forms">
+      <SignupForm />
+      <SigninForm />
+      <Signout />
+      {loginUser ? <Contents /> : null}
       </div>
     </div>
   );
 }
 
-export default App; 
+export default App;
---
+++ src/Contents.tsx
@@ -0,0 +1,10 @@
+export const Contents: React.FC = () => {
+    return (
+      <div>
+          <p>You are logged in!</p>
+      </div>
+    );
+  };
+  
+  export default Contents;
+  
